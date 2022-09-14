bb13134517be03b000e2adbf1d45cef4edffe3ce
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-09-14 12:51:07 +0100

    Add swagger and profiles

--- src/main/java/com/qa/studentservicemysql/config/AppConfig.java
+++ src/main/java/com/qa/studentservicemysql/config/AppConfig.java
@@ -4,6 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 @Configuration
 public class AppConfig {
 	
@@ -11,5 +16,14 @@ public class AppConfig {
 	public ModelMapper mapper() {
 		return new ModelMapper();
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.qa.studentservicemysql.controller"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
